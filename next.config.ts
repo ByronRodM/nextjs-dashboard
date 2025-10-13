135b22d139d45bb87fc3ec0cd906a473240d3327
Byron Rodríguez <brodriguezm6@miumg.edu.gt>
2025-10-13 01:39:23 -0600

    feat: Añadir soporte para Partial Prerendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        cacheComponents: true,
+    } as any,
 };
 
 export default nextConfig;
